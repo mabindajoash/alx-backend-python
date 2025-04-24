e020097279c0d2a31baf1bdebdc53bab19661414
mabindajoash <mabindajoash@gmail.com>
2025-04-24 16:45:30 +0000

    task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """module demonstrating duck typing"""
-from typing import Sequence, Tuple, Any, List
+from typing import Sequence, Tuple, Any, List, Iterable
 
 
-def element_length(lst: Sequence[Any]) -> List[Tuple[Any, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """function to demonstrate duck typing"""
     return [(i, len(i)) for i in lst]
