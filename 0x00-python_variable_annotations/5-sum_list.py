225a6b78426dbe13151a43af23168ffffa9d2a42
mabindajoash <mabindajoash@gmail.com>
2025-04-24 15:35:51 +0000

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module containing sum_list function"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """takes a list of floats and returns the sum"""
+    return sum(input_list)
