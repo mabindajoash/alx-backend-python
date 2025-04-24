1a9c4bfc3892c3bc31445010313e54ede064e1ea
mabindajoash <mabindajoash@gmail.com>
2025-04-24 16:54:48 +0000

    task 100

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""module demonstrating duck typed annotations"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
