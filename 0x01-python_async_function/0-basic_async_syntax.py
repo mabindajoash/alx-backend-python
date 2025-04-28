577ab396dde890a1f1da5010bf2fd9859f107786
mabindajoash <mabindajoash@gmail.com>
2025-04-28 17:33:25 +0000

    task 1 correction 2

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-"""module containing max_delay function"""
-
-import random
+'''Task 0's module.
+'''
 import asyncio
+import random
 
 
-async def wait_random(max_delay=10):
-    """wait for a random time and return time"""
-    delay = random.random() * max_delay
-    await asyncio.sleep(delay)
-    return delay
+async def wait_random(max_delay: int = 10) -> float:
+    '''Waits for a random number of seconds.
+    '''
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
